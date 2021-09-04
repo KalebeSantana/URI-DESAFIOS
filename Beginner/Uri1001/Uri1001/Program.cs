9d57359b292ad2775387da89e3f99acfb085aba9
KalebeSantana <santana.abrantes01@gmail.com>
2021-09-03 22:41:08 -0300

    1001

---
+++ Beginner/Uri1001/Uri1001/Program.cs
@@ -0,0 +1,33 @@
+﻿//Leia 2 variáveis, denominadas A e B e fazer a soma dessas duas variáveis, atribuindo o resultado à variável X .
+//Imprima X conforme mostrado abaixo. Imprima a linha final após o resultado, caso contrário, você obterá “ Erro de apresentação ”.
+
+//Entrada
+//O arquivo de entrada conterá 2 números inteiros.
+
+//Saída
+//Imprime a letra X (maiúscula) com um espaço em branco antes e depois do sinal de igual seguido do valor de X, conforme exemplo a seguir.
+
+//Obs .: afinal não se esqueça da linha final.
+
+
+using System;
+
+namespace Uri1001
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int A = int.Parse(Console.ReadLine());
+            int B = int.Parse(Console.ReadLine());
+
+            int X = A + B;
+
+            Console.WriteLine("X = {0}", X);
+
+            Console.ReadLine();
+        }
+
+    }
+}
