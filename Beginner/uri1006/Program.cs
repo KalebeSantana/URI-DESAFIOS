e3ffceb78e0d28337d777a73d1fb8c11331017a3
KalebeSantana <santana.abrantes01@gmail.com>
2021-09-06 15:41:48 -0300

    1005 to 1008

---
+++ Beginner/uri1006/Program.cs
@@ -0,0 +1,32 @@
+﻿//Leia 3 valores, no caso, variáveis A, B e C, que são as três notas de um aluno. A seguir, calcule a média do aluno, 
+//sabendo que a nota A tem peso 2, a nota B tem peso 3 e a nota C tem peso 5. Considere que cada nota pode ir de 0 até 10.0,
+//sempre com uma casa decimal.
+
+//Entrada
+//O arquivo de entrada contém 3 valores com uma casa decimal, de dupla precisão (double).
+
+//Saída
+//Imprima a mensagem "MEDIA" e a média do aluno conforme exemplo abaixo,
+//com 1 dígito após o ponto decimal e com um espaço em branco antes e depois da igualdade. 
+
+using System;
+
+namespace uri1006
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            double A, B, C, media;
+
+            A = double.Parse(Console.ReadLine());
+            B = double.Parse(Console.ReadLine());
+            C = double.Parse(Console.ReadLine());
+
+            media = ((A * 2.0) + (B * 3.0) + (C * 5.0)) / 10.0;
+
+            Console.WriteLine("MEDIA = " + media.ToString("F1"));
+        }
+    }
+}
