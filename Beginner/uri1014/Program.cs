4d108866ef87b35e0114980970dd7949dc03fd95
KalebeSantana <santana.abrantes01@gmail.com>
2021-09-06 23:48:35 -0300

    1014 and 1015

---
+++ Beginner/uri1014/Program.cs
@@ -0,0 +1,26 @@
+﻿//Calcule o consumo médio de um automóvel sendo fornecidos a distância total percorrida (em Km) e o total de combustível gasto (em litros).
+
+//Entrada
+//O arquivo de entrada contém dois valores: um valor inteiro X representando a distância total percorrida (em Km),
+//e um valor real Y representando o total de combustível gasto, com um dígito após o ponto decimal.
+
+//Saída
+//Apresente o valor que representa o consumo médio do automóvel com 3 casas após a vírgula, seguido da mensagem "km/l".
+
+using System;
+
+namespace uri1014
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int X = int.Parse(Console.ReadLine());
+            double Y = double.Parse(Console.ReadLine());
+
+            double consumo =  X / Y;
+
+            Console.WriteLine($"{consumo:F3} km/l");
+        }
+    }
+}
---
+++ Beginner/uri1015/Program.cs
@@ -0,0 +1,39 @@
+﻿//Leia os quatro valores correspondentes aos eixos x e y de dois pontos quaisquer no plano, p1(x1, y1) e p2(x2, y2) 
+//e calcule a distância entre eles, mostrando 4 casas decimais após a vírgula, segundo a fórmula:
+
+//Distancia =
+
+//Entrada
+//O arquivo de entrada contém duas linhas de dados. A primeira linha contém dois valores de ponto flutuante: 
+//x1 y1 e a segunda linha contém dois valores de ponto flutuante x2 y2.
+
+//Saída
+//Calcule e imprima o valor da distância segundo a fórmula fornecida, com 4 casas após o ponto decimal.
+
+using System;
+
+namespace uri1015
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double x1, x2, y1, y2, distancia;
+
+            string[] vet = Console.ReadLine().Split(' ');
+            x1 = double.Parse(vet[0]);
+            y1 = double.Parse(vet[1]);
+
+            vet = Console.ReadLine().Split(' ');
+            x2 = double.Parse(vet[0]);
+            y2 = double.Parse(vet[1]);
+
+            distancia = Math.Sqrt(Math.Pow(x2 - x1, 2) + (Math.Pow(y2 - y1, 2)));
+
+            Console.WriteLine(distancia.ToString("F4"));
+
+
+
+        }
+    }
+}
